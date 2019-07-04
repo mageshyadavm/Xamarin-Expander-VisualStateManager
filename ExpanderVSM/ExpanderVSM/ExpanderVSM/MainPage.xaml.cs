9a42eb3cf34d1ca9c6944dc814de21d042173fb6
Sangeetha Raju <sangeetha.raju@syncfusion.com>
2019-07-04 15:01:40 +0530

    VSM sample for SfExpander

---
+++ ExpanderVSM/ExpanderVSM/ExpanderVSM/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Syncfusion.XForms.Expander;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ExpanderVSM
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            //SfExpander expander = new SfExpander();
+            //expander.Header = new Label() { Text = "Veg Pizza" };
+            //expander.Content = new Label() { Text = "Veg pizza is prepared with the items that meet vegetarian standards by not including any meat or animal tissue products." };
+            //VisualStateGroupList visualStateGroupList = new VisualStateGroupList();
+            //VisualStateGroup commonStateGroup = new VisualStateGroup();
+
+            //VisualState expanded = new VisualState
+            //{
+            //    Name = "Expanded"
+            //};
+
+            //expanded.Setters.Add(new Setter { Property = SfExpander.HeaderBackgroundColorProperty, Value = Color.Red });
+            //expanded.Setters.Add(new Setter { Property = SfExpander.HeaderBackgroundColorProperty, Value = Color.Red });
+
+            //VisualState collapsed = new VisualState
+            //{
+            //    Name = "Collapsed"
+            //};
+            //collapsed.Setters.Add(new Setter { Property = SfExpander.HeaderBackgroundColorProperty, Value = Color.Green });
+            //collapsed.Setters.Add(new Setter { Property = SfExpander.HeaderBackgroundColorProperty, Value = Color.Green });
+            //commonStateGroup.States.Add(expanded);
+            //commonStateGroup.States.Add(collapsed);
+
+            //visualStateGroupList.Add(commonStateGroup);
+            //VisualStateManager.SetVisualStateGroups(expander, visualStateGroupList);
+            //this.Content = expander;
+        }
+    }
+}
